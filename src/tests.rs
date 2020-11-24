60ab9b06281430d0d40cc44e63c4b6a7adde169f
Тимофей <tymcrit@gmail.com>
2020-11-24 20:43:20 +0300

    Tests moved in other directory

--- src/tests.rs
+++ src/tests.rs
@@ -1,7 +1,9 @@
 use crate::{Code, Vars};
-use std::fs::File;
-use std::io::prelude::*;
-use std::sync::{Arc, Mutex};
+use std::{
+    fs::File,
+    io::prelude::*,
+    sync::{Arc, Mutex},
+};
 
 fn run_test(test_name: &str) {
     let mut s = String::new();
@@ -40,60 +42,60 @@ fn run_test(test_name: &str) {
 
 #[test]
 fn test_puts() {
-    run_test("puts");
+    run_test("core/core/puts");
 }
 
 #[test]
 fn test_copy() {
-    run_test("copy");
+    run_test("core/core/copy");
 }
 
 #[test]
 fn test_call_string() {
-    run_test("call_string");
+    run_test("core/core/call_string");
 }
 
 #[test]
 fn test_call_object() {
-    run_test("call_object");
+    run_test("core/core/call_object");
 }
 
 #[test]
 fn test_call_closure() {
-    run_test("call_closure");
+    run_test("core/core/call_closure");
 }
 
 #[test]
 fn test_closure_args() {
-    run_test("closure_args");
+    run_test("core/core/closure_args");
 }
 
 #[test]
 fn test_call_raw_closure() {
-    run_test("call_raw_closure")
+    run_test("core/core/call_raw_closure");
 }
 
 #[test]
 fn test_closure_scope() {
-    run_test("closure_scope")
+    run_test("core/core/closure_scope");
 }
 
 #[test]
 fn test_quoted_strings() {
-    run_test("quoted_strings")
+    run_test("core/core/quoted_strings");
 }
 
 #[test]
 fn test_multiple_assign() {
-    run_test("multiple_assign")
+    run_test("core/core/multiple_assign");
 }
 
 #[test]
 fn test_nested_assign() {
-    run_test("nested_assign")
+    run_test("core/core/nested_assign");
 }
 
 #[test]
 fn test_tuple_puts() {
-    run_test("tuple_puts")
+    run_test("core/core/tuple_puts");
 }
