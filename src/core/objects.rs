c132ab0f407bae6bb2f926cf0bc156743256c688
Тимофей <tymcrit@gmail.com>
2020-11-24 21:35:02 +0300

    Some string and tuple methods are added

--- src/core/objects.rs
+++ src/core/objects.rs
@@ -7,8 +7,71 @@ impl Object for String {
         Box::new(std::clone::Clone::clone(self))
     }
 
-    fn call(self: Box<Self>, _params: Vars, _scope: &mut Vec<Vars>) -> Box<dyn Object> {
-        self
+    fn call(mut self: Box<Self>, mut params: Vars, scope: &mut Vec<Vars>) -> Box<dyn Object> {
+        if params.contains("1") {
+            let method = params.get(&scope, "1").to_string();
+            match &method[..] {
+                "len" => Box::new(self.len().to_string()),
+
+                "_len" => {
+                    let len = Box::new(self.len().to_string());
+                    Box::new(vec![self as Box<dyn Object>, len])
+                }
+
+                "split" => {
+                    let delimiter = if params.contains("2") {
+                        params.get(&scope, "2").to_string()
+                    } else {
+                        " ".to_string()
+                    };
+
+                    Box::new(
+                        self.split(&delimiter)
+                            .map(|x| Box::new(x.to_string()) as Box<dyn Object>)
+                            .collect::<Vec<_>>(),
+                    )
+                }
+
+                "push" => {
+                    let str_to_push = if params.contains("2") {
+                        params.get(&scope, "2").to_string()
+                    } else {
+                        "".to_string()
+                    };
+                    self.push_str(&str_to_push);
+
+                    self
+                }
+
+                "eq" => {
+                    let str_to_compare = if params.contains("2") {
+                        params.get(&scope, "2").to_string()
+                    } else {
+                        panic!("Expected 1 argument, found 0");
+                    };
+
+                    Box::new((*self == str_to_compare).to_string())
+                }
+
+                "_eq" => {
+                    let str_to_compare = if params.contains("2") {
+                        params.get(&scope, "2").to_string()
+                    } else {
+                        panic!("Expected 1 argument, found 0");
+                    };
+
+                    let r = Box::new((*self == str_to_compare).to_string());
+
+                    Box::new(vec![self as Box<dyn Object>, Box::new(str_to_compare), r])
+                }
+
+                other => {
+                    panic!("Unknown method: {}", other);
+                }
+            }
+        } else {
+            self
+        }
     }
 
     fn to_string(self: Box<Self>) -> String {
@@ -29,8 +92,50 @@ impl Object for Vec<Box<dyn Object>> {
         Box::new(cloned)
     }
 
-    fn call(self: Box<Self>, _params: Vars, _scope: &mut Vec<Vars>) -> Box<dyn Object> {
-        self
+    fn call(mut self: Box<Self>, mut params: Vars, scope: &mut Vec<Vars>) -> Box<dyn Object> {
+        if params.contains("1") {
+            let method = params.get(&scope, "1").to_string();
+            match &method[..] {
+                "push" => {
+                    let val = if params.contains("2") {
+                        params.get(&scope, "2")
+                    } else {
+                        panic!("Expected 1 argument, found 0")
+                    };
+                    self.push(val);
+                    self
+                }
+
+                "pop" => {
+                    let el = self
+                        .pop()
+                        .unwrap_or_else(|| panic!("Can't pop value from empty tuple"));
+                    Box::new(vec![self as Box<dyn Object>, el])
+                }
+
+                "is_empty" => Box::new(self.is_empty().to_string()),
+
+                "_is_empty" => {
+                    let r = Box::new(self.is_empty().to_string());
+                    Box::new(vec![self as Box<dyn Object>, r])
+                }
+
+                other => {
+                    let i = other
+                        .parse::<usize>()
+                        .unwrap_or_else(|_| panic!("Unknown method: {}", other));
+                    Object::clone(
+                        self.get(i)
+                            .unwrap_or_else(|| {
+                                panic!("Index out of bounds: index is {}, len is {}", i, self.len())
+                            })
+                            .as_ref(),
+                    )
+                }
+            }
+        } else {
+            self
+        }
     }
 
     fn to_string(self: Box<Self>) -> String {
