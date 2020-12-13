e4292798add41da32e451305395061fa471376d6
Тимофей <tymcrit@gmail.com>
2020-12-13 19:10:49 +0300

    File name is reading from command line arguments instead of stdin

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,19 @@ mod stdlib;
 #[macro_use]
 extern crate pest_derive;
 
-use std::{fs::File, io::prelude::*};
+use std::{env::args_os, fs::File, io::prelude::*};
 
 use crate::core::*;
 
 fn main() {
+    let mut file = File::open(
+        args_os()
+            .nth(1)
+            .unwrap_or_else(|| panic!("Expected 1 argument, found 0")),
+    )
+    .unwrap();
     let mut s = String::new();
-    std::io::stdin().read_line(&mut s).unwrap();
-    let mut f = File::open(s.trim().to_string()).unwrap();
-    let mut s = String::new();
-    f.read_to_string(&mut s).unwrap();
+    file.read_to_string(&mut s).unwrap();
 
     let (vars, mut scope) = stdlib::stdlib(std::io::stdout(), std::io::stdin());
     println!("{}", Code::from_string(s).run(vars, &mut scope).to_string());
